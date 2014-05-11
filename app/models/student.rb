53eaf7bd779ef8775ce9ac82e2fe7d6174831207
jamesj223 <jamesj223@gmail.com>
2014-05-12 00:04:50 +1000

    Implemented Email Confirmation System. Users must now click on a link that is sent to their @student.unimelb address in order to activate their account.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@student\.unimelb\.edu\.au\z/,
                   message: "must be a '@student.unimelb.edu.au' address" }
@@ -10,5 +10,5 @@ class Student < ActiveRecord::Base
   def full_name
 	"#{first_name} #{last_name}"
   end
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140511115945) do
+ActiveRecord::Schema.define(version: 20140511134333) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -39,8 +39,12 @@ ActiveRecord::Schema.define(version: 20140511115945) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "students", ["confirmation_token"], name: "index_students_on_confirmation_token", unique: true
   add_index "students", ["email"], name: "index_students_on_email", unique: true
   add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
 
