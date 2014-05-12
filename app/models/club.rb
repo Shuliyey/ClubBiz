fce14a06f8d25955c9f49091672484c380c8fdf4
jamesj223 <jamesj223@gmail.com>
2014-05-12 19:08:08 +1000

    Cleanup

--- app/models/club.rb
+++ app/models/club.rb
@@ -2,5 +2,10 @@ class Club < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def full_name
+	"#{name}"
+  end
+
 end
