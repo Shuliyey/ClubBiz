1bab5daa57f5bdefb9bb5c7fe17d79d26200b713
jamesj223 <jamesj223@gmail.com>
2014-05-22 00:45:10 +1000

    Added TODO list. Other minor changes.

--- app/controllers/clubs_students_controller.rb
+++ app/controllers/clubs_students_controller.rb
@@ -2,14 +2,14 @@ class ClubsStudentsController < ApplicationController
 
   	# Clubs
   	def create #like Club
-	  	@student = Student.find(params[:student_id])
+	  	@student = Student.find(current_student.id)
 	  	@club = Club.find(params[:club_id])
 	  	@student.clubs << @club
 		redirect_to profile_path(@club.id)
 	end
 
 	def destroy #unlike Club
-	  	@student = Student.find(params[:student_id])
+	  	@student = Student.find(current_student.id)
 	  	@club = Club.find(params[:club_id])
 	  	@student.clubs.delete(@club)
 		redirect_to profile_path(@club.id)
--- app/controllers/events_students_controller.rb
+++ app/controllers/events_students_controller.rb
@@ -2,14 +2,14 @@ class EventsStudentsController < ApplicationController
 
 	# Events
   	def create #like event 
-	  	@student = Student.find(params[:student_id])
+	  	@student = Student.find(current_student.id)
 	  	@event = Event.find(params[:event_id])
 	  	@student.events << @event
 		redirect_to @event
   	end
 
 	def destroy #unlike event
-	  	@student = Student.find(params[:student_id])
+	  	@student = Student.find(current_student.id)
 	  	@event = Event.find(params[:event_id])
 	  	@student.events.delete(@event)
 		redirect_to @event
