0be992359f32fb27a2da8ea7fe73c38107c39b79
David Finn <dsfinn@gmail.com>
2014-05-25 20:30:38 +1000

    Fixed the reservations so that they worked across botht he event view and the student view

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,13 +11,17 @@ end
 
 def destroy #remove a reservation for an event
 	@student = Student.find(current_student.id)
-	@event = Event.find(params[:event_id])
-	@reservation = @student.reservations.where(:event_id => @event.id)
+	@reservation = Reservation.find(params[:id])
+	@event = Event.find(@reservation.event_id)
 	@student.reservations.delete(@reservation)
 	@event.reservations.delete(@reservation)
 	redirect_to @event
 end
 
 private
+
+def reservation_params
+      params.require(:event_id, :student_id)
+end
 
 end
