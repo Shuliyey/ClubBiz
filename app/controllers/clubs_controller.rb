82d021546da16be245a1f30dbf27e68360f8c190
Shuliyey <Shuliyey@gmail.com>
2014-05-25 20:03:48 +1000

    limit the Event display in Clubs for the next 4 weeks

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -2,7 +2,9 @@ class ClubsController < ApplicationController
 
 	def show
 	  @club = Club.find(params[:id])
-	  @events = Event.where(:club_id => params[:id])
+	  date_from = Time.zone.now.beginning_of_day
+	  date_to = date_from + 4.week
+	  @events = Event.where(:club_id => params[:id], :when=> date_from..date_to)
 	end
 
 	def index 
